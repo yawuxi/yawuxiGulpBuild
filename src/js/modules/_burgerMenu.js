f03604b6cc0a9b847b222edf2fbcef63b27c6770
yawuxi <ryliov.work@gmail.com>
2021-11-10 19:02:09 +0200

    сделал экспорт с помощью ESModules

--- src/js/modules/_burgerMenu.js
+++ src/js/modules/_burgerMenu.js
@@ -1,20 +1,26 @@
 //burgerMenu
-const burgerBtn = document.querySelector('.burger-menu__content'),
-	burgerMenu = document.querySelector('.header__navigation'),
-	header = document.querySelector('.header'),
-	body = document.body;
+function burgerMenu(params) {
+	const burgerBtn = document.querySelector('.burger-menu__content'),
+		burgerMenu = document.querySelector('.header__navigation'),
+		header = document.querySelector('.header'),
+		body = document.body;
 
-burgerBtn.addEventListener('click', () => {
-	burgerBtn.classList.toggle('active');
-	burgerMenu.classList.toggle('active');
-	body.classList.toggle('lock');
-	if (document.documentElement.offsetWidth > 768) {
-		if (header.style.paddingRight == '17px' && body.style.paddingRight == '17px') {
-			header.style.paddingRight = '';
-			body.style.paddingRight = '';
-		} else {
-			header.style.paddingRight = '17px';
-			body.style.paddingRight = '17px';
+	burgerBtn.addEventListener('click', () => {
+		burgerBtn.classList.toggle('active');
+		burgerMenu.classList.toggle('active');
+		body.classList.toggle('lock');
+		if (document.documentElement.offsetWidth > 768) {
+			if (header.style.paddingRight == '17px' && body.style.paddingRight == '17px') {
+				header.style.paddingRight = '';
+				body.style.paddingRight = '';
+			} else {
+				header.style.paddingRight = '17px';
+				body.style.paddingRight = '17px';
+			}
 		}
-	}
-});
+	});
+}
+
+export {
+	burgerMenu
+};
